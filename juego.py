5c836b97fdefe3485a07be5ca32ad8fd5fee2856
Santiago <s.perezj19@gmail.com>
2023-09-13 16:18:03 -0500

    2 actualizacion

--- juego.py
+++ juego.py
@@ -12,6 +12,18 @@ Simbolo in Operacion -> Ayuda!
 Simbolo not in Operacion -> No Existe!
 """
 
+colors = {
+    'green': '\033[92m',
+    'yellow': '\033[93m',
+    'red': '\033[91m',
+    'ENDC': '\033[0m',
+ }
+
+def color_letter(letter,color):
+    return colors[color] + letter + colors['ENDC']
+
+
+
 # init game
 print("Nerdle.py empezar!")
 
@@ -22,7 +34,7 @@ for i in range(6):
     board.append(['_' for l in range(6)])
 
 game_loop_counter = 0
-while not win:
+while (not win) and ():
     text = input("")
     while len(text) != len(Operacion):
         print(f"la operacion debe tener {len(Operacion)} de caracteres")
@@ -32,6 +44,21 @@ while not win:
     if Operacion == text:
         board[game_loop_counter] = [l for l in text]
         win = True
+
+    #letter in word
+
+    else:
+        test_line = []
+        for j in range(len(Operacion)):
+            if text[j] == Operacion[j]:
+             if text[j] in Operacion:
+                 test_line.append(text[j])
+            elif text[j] in Operacion:
+                test_line.append(color_letter(text[j],'yellow'))
+
+            else:
+                test_line.append(color_letter(text[j], 'red'))
+        board[game_loop_counter] = test_line
 
     # Draw
     for i in range(6):
